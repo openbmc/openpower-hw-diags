218ceec7e54a75fdf93778b3dfb33a15a64e476f
Zane Shelley <zshelle@us.ibm.com>
2019-06-19 20:14:44 -0500

    Initial Attention Handler design
    In response to notifications from the BMC, the Attention Handler (ATTN) provides
    support for platform error handling and runtime diagnostic. ATTN utilizes
    hardware status and data, and interacts with other firmware components to meet
    the overall platform error handling objectives.
    
    Signed-off-by: Zane Shelley <zshelle@us.ibm.com>
    Change-Id: I2a30ec6258e89092d118d61ea9c482d0d40927d9

---
+++ attn/attn_main.cpp
@@ -0,0 +1,13 @@
+#include <attn_handler.hpp>
+
+/**
+ * @brief Attention handler application main()
+ *
+ * This is the main interface to the Attention handler application.
+ */
+int main()
+{
+    attnHandler(); // handle active attentions
+
+    return 0;
+}
