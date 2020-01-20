b399e1846728f3b74692f29cf325ca84f6cd5a5a
Ben Tyner <ben.tyner@ibm.com>
2020-01-20 11:29:31 -0600

    Add event logging support for attention handler.
    The attention handler will add entries to the event log for error
    conditions such as checkstop and vital attentions. Additional data will
    be included in the event log entry so that the entries can be used to
    create detailed log reports.
    
    Signed-off-by: Ben Tyner <ben.tyner@ibm.com>
    Change-Id: I1b9ec5f8c46ae9b6d96e041e50149b4fcfc4e5f3

---
+++ attn/attn_logging.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <map>
+
+namespace attn
+{
+/**
+ * @brief Add entry to the attention handler event log
+ *
+ * @param i_message     The message property of the event log object. For PEL
+ *                      creation this also correlates to the error name in the
+ *                      messge registry.
+ *
+ * @param i_additional  AdditionalData property of the event log object. For
+ *                      PEL creation this can be used to proved SRC data
+ *                      words 6-9.
+ */
+void logEntry(const std::string& i_message,
+              const std::map<std::string, std::string>& i_additional);
+
+} // namespace attn
