b4325c144d9eb7fc8b7e9e961304503711f7d204
Ben Tyner <ben.tyner@ibm.com>
2020-01-20 11:29:31 -0600

    Add event logging support for attention handler.
    The attention handler will add entries to the event log for error
    conditions such as checkstop and vital attentions. Additional data will
    be included in the event log entry so that the entries can be used to
    create detailed log reports.
    
    Signed-off-by: Ben Tyner <ben.tyner@ibm.com>
    Change-Id: I1b9ec5f8c46ae9b6d96e041e50149b4fcfc4e5f3

---
+++ attn/attn_logging.cpp
@@ -0,0 +1,34 @@
+#include <unistd.h>
+
+#include <sdbusplus/bus.hpp>
+
+#include <map>
+#include <string>
+
+namespace attn
+{
+
+/**
+ * @brief Add entry to the attention handler event log
+ */
+void logEntry(const std::string& i_message,
+              const std::map<std::string, std::string>& i_additional)
+{
+
+    // Get access to logging interface and method for creating log entries
+    auto bus    = sdbusplus::bus::new_default_system();
+    auto method = bus.new_method_call(
+        "xyz.openbmc_project.Logging", "/zyz/openbmc_project/logging",
+        "xyz.openbmc_project.Logging.Create", "Create");
+
+    // Append log entry message, severity level, additional data
+    method.append(i_message, "xyz.openbmc_project.Logging.Entry.Level.Error",
+                  i_additional);
+
+    // Add event to log
+    auto reply = bus.call(method);
+
+    return;
+}
+
+} // namespace attn
