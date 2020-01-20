cf86cf4482353033d1f3d0bada8a35b2303247c1
Ben Tyner <ben.tyner@ibm.com>
2020-01-20 11:29:31 -0600

    Add event logging support for attention handler.
    The attention handler will add entries to the event log for error
    conditions such as checkstop and vital attentions. Additional data will
    be included in the event log entry so that the entries can be used to
    create detailed log reports.
    
    Signed-off-by: Ben Tyner <ben.tyner@ibm.com>
    Change-Id: I1b9ec5f8c46ae9b6d96e041e50149b4fcfc4e5f3

--- attn/attn_handler.cpp
+++ attn/attn_handler.cpp
@@ -1,5 +1,6 @@
 #include <libpdbg.h>
 
+#include <attn_logging.hpp>
 #include <phosphor-logging/log.hpp>
 #include <sdbusplus/bus.hpp>
 
@@ -15,14 +16,16 @@ namespace attn
  *
  * @return 0 = success
  */
-int handleVital();
+int handleVital(const uint32_t i_proc, const uint32_t i_cfam_1007,
+                const uint32_t i_cfam_100d);
 
 /**
  * @brief Handle checkstop attention
  *
  * @return 0 = success
  */
-int handleCheckstop();
+int handleCheckstop(const uint32_t i_proc, const uint32_t i_cfam_1007,
+                    const uint32_t i_cfam_100d);
 
 /**
  * @brief Handle special attention
@@ -56,6 +59,7 @@ void attnHandler()
         {
             proc = pdbg_target_index(target); // get processor number
 
+            // trace message
             std::stringstream ss; // log message stream
             ss << "[ATTN] checking processor " << proc << std::endl;
             log<level::INFO>(ss.str().c_str());
@@ -63,12 +67,14 @@ void attnHandler()
             // get active attentions on processor
             if (0 != fsi_read(target, 0x1007, &isr_val))
             {
+                // trace message
                 std::stringstream ss; // log message stream
                 ss << "[ATTN] Error! cfam read 0x1007 FAILED" << std::endl;
                 log<level::INFO>(ss.str().c_str());
             }
             else
             {
+                // trace message
                 std::stringstream ss; // log message stream
                 ss << "[ATTN] cfam 0x1007 = 0x";
                 ss << std::hex << std::setw(8) << std::setfill('0');
@@ -78,12 +84,14 @@ void attnHandler()
                 // get interrupt enabled special attentions mask
                 if (0 != fsi_read(target, 0x100d, &isr_mask))
                 {
+                    // trace message
                     std::stringstream ss; // log message stream
                     ss << "[ATTN] Error! cfam read 0x100d FAILED" << std::endl;
                     log<level::INFO>(ss.str().c_str());
                 }
                 else
                 {
+                    // trace message
                     std::stringstream ss; // log message stream
                     ss << "[ATTN] cfam 0x100d = 0x";
                     ss << std::hex << std::setw(8) << std::setfill('0');
@@ -93,13 +101,13 @@ void attnHandler()
                     // bit 0 on "left": bit 30 = SBE vital attention
                     if (isr_val & isr_mask & 0x00000002)
                     {
-                        handleVital();
+                        handleVital(proc, isr_val, isr_mask);
                     }
 
                     // bit 0 on "left": bit 1 = checkstop
                     if (isr_val & isr_mask & 0x40000000)
                     {
-                        handleCheckstop();
+                        handleCheckstop(proc, isr_val, isr_mask);
                     }
 
                     // bit 0 on "left": bit 2 = special attention
@@ -118,20 +126,22 @@ void attnHandler()
 /**
  * @brief Handle SBE vital attention
  */
-int handleVital()
+int handleVital(const uint32_t i_proc, const uint32_t i_cfam_1007,
+                const uint32_t i_cfam_100d)
 {
-    int rc = 1; // vital attention handling not yet supported
+    int rc = 0; // vital attention supported
 
+    // Trace message
     std::stringstream ss; // log message stream
     ss << "[ATTN] vital" << std::endl;
     log<level::INFO>(ss.str().c_str());
 
-    if (0 != rc)
-    {
-        std::stringstream ss; // log message stream
-        ss << "[ATTN] vital NOT handled" << std::endl;
-        log<level::INFO>(ss.str().c_str());
-    }
+    // Additional log entry data can be stored as key/value pairs
+    std::map<std::string, std::string> additional;
+    additional["ATTN_PROC"] = std::to_string(i_proc);
+    additional["CFAM_1007"] = std::to_string(i_cfam_1007);
+    additional["CFAM_100D"] = std::to_string(i_cfam_100d);
+    logEntry("org.open_power.hw_diags.Error.Vital", additional);
 
     return rc;
 }
@@ -139,20 +149,22 @@ int handleVital()
 /**
  * @brief Handle checkstop attention
  */
-int handleCheckstop()
+int handleCheckstop(const uint32_t i_proc, const uint32_t i_cfam_1007,
+                    const uint32_t i_cfam_100d)
 {
-    int rc = 1; // checkstop handling not yet supported
+    int rc = 0; // checkstop attention supported
 
+    // Trace message
     std::stringstream ss; // log message stream
     ss << "[ATTN] checkstop" << std::endl;
     log<level::INFO>(ss.str().c_str());
 
-    if (0 != rc)
-    {
-        std::stringstream ss; // log message stream
-        ss << "[ATTN] checkstop NOT handled" << std::endl;
-        log<level::INFO>(ss.str().c_str());
-    }
+    // Log entry
+    std::map<std::string, std::string> additional;
+    additional["ATTN_PROC"] = std::to_string(i_proc);
+    additional["CFAM_1007"] = std::to_string(i_cfam_1007);
+    additional["CFAM_100D"] = std::to_string(i_cfam_100d);
+    logEntry("org.open_power.hw_diags.Error.Checkstop", additional);
 
     // TODO recoverable errors?
 
@@ -161,16 +173,20 @@ int handleCheckstop()
 
 /**
  * @brief Handle special attention
+ *
+ * Currently we are only handling PHYP breakpoint in which case we will
+ * add a trace message to the journal and notify the breakpoint handler
+ * of the event.
  */
 int handleSpecial()
 {
     int rc = 0; // special attention handling supported
 
+    // Trace message
     std::stringstream ss; // log message stream
-
     ss << "[ATTN] special" << std::endl;
 
-    // Currently we are only handling Cronus breakpoints
+    // Trace message
     ss << "[ATTN] breakpoint" << std::endl;
     log<level::INFO>(ss.str().c_str());
 
