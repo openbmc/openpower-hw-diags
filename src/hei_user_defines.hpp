8ef2425d185287f21f089509681cbeb766a0f09e
Ben Tyner <ben.tyner@ibm.com>
2020-02-05 18:11:02 -0600

    Integrate openpower-libhei with openpower-hw-diags
    Pull down openpower-libhei as a subproject and build the isolator
    support into openpower-hw-diags.
    
    Signed-off-by: Ben Tyner <ben.tyner@ibm.com>
    Change-Id: I6eeacc802363fec6f0f113e75eb268a8c1774b7c

---
+++ src/hei_user_defines.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+/**
+ * @file hei_user_defines.hpp
+ * @brief The purpose of this file is to create common defines that
+ *        will be used throughout this library.
+ */
+
+#include <assert.h>
+#include <inttypes.h>
+#include <stdio.h>
+
+#define HEI_INF(...)                                                           \
+    {                                                                          \
+        printf("HWDIAGS I> " __VA_ARGS__);                                     \
+        printf("\n");                                                          \
+    }
+
+#define HEI_ERR(...)                                                           \
+    {                                                                          \
+        printf("HWDIAGS E> " __VA_ARGS__);                                     \
+        printf("\n");                                                          \
+    }
+
+#define HEI_ASSERT(expression) assert(expression);
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <hei_main.hpp>
 
 #include <stdio.h>
 
