06e4401fdb49a9fae575d31f15a01e13048f6db0
Ben Tyner <ben.tyner@ibm.com>
2020-02-24 10:24:47 -0600

    Combine attn handler and openpower hwdiags
    The main binary is now openpower-hw-diags. This application will take a
    command line option --daemon to load it as a daemon. As a daemon it will
    register the attention handler portion of application as the attention
    gpio event handler. If the application is not loaded as a daemon it will
    operate as a stand alone application which accepts command line options
    for requesting hardware analyses and diagnostics operations.
    
    Change-Id: I6210b744cb320873d74a0757928f904ca6296846
    Signed-off-by: Ben Tyner <ben.tyner@ibm.com>

---
+++ analyzer/analyzer_main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#include <hei_main.hpp>
+
+namespace analyzer
+{
+
+/** @brief Analyze and isolate hardware errors */
+void analyzeHardware()
+{
+    printf("analyzeHardware()\n");
+}
+
+} // namespace analyzer
