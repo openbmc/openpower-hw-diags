5a700693b3076a74a589e73546139d5e312ef4ab
Ben Tyner <ben.tyner@ibm.com>
2020-05-08 18:52:48 -0500

    Platform Event Log (PEL) generation support added
    Attention events will now generate an associated PEL
    
    Signed-off-by: Ben Tyner <ben.tyner@ibm.com>
    Change-Id: I8d2c73657cc7eced93e6465c391c0a600c4278fa

--- analyzer/analyzer_main.cpp
+++ analyzer/analyzer_main.cpp
@@ -4,22 +4,37 @@ namespace analyzer
 {
 
 /** Analyze error condition using the hardware error isolator */
-void analyzeHardware()
+int analyzeHardware(std::map<std::string, std::string>& i_errors)
 {
     using namespace libhei;
 
     std::vector<Chip> chipList; // data to isolator
     IsolationData isoData{};    // data from isolator
 
-    // TEMP CODE - start
+    // FIXME TEMP CODE - start
+
     initialize(nullptr, 0);
 
     chipList.emplace_back(Chip{"proc", static_cast<ChipType_t>(0xdeadbeef)});
 
-    isolate(chipList, isoData);
+    isolate(chipList, isoData); // isolate errors
+
+    if (!(isoData.getSignatureList().empty()))
+    {
+        // Signature signature = isoData.getSignatureList().back();
+    }
+    else
+    {
+        std::string signature = "0xfeed";
+        std::string chip      = "0xbeef";
+        i_errors[signature]   = chip;
+    }
 
     uninitialize();
-    // TEMP CODE - end
+
+    // FIXME TEMP CODE - end
+
+    return 0; // FIXME - error/success from isolator or isolation data
 }
 
 } // namespace analyzer
