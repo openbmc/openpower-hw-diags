5c02504e350d95d414fe8f3b70ad0b8f8713851e
Zane Shelley <zshelle@us.ibm.com>
2020-05-08 14:35:15 -0500

    Updates to HEI user intefaces
    Signed-off-by: Zane Shelley <zshelle@us.ibm.com>
    Change-Id: Ic146ae7d212df4cc5be876b636481ba908c130b6

--- analyzer/user_interface.cpp
+++ analyzer/user_interface.cpp
@@ -10,30 +10,30 @@ namespace libhei
 
 //------------------------------------------------------------------------------
 
-ReturnCode registerRead(const Chip& i_chip, void* o_buffer, size_t& io_bufSize,
+bool registerRead(const Chip& i_chip, void* o_buffer, size_t& io_bufSize,
                         uint64_t i_regType, uint64_t i_address)
 {
-    ReturnCode rc = RC_SUCCESS;
+    bool accessFailure = false;
 
     //    HEI_INF("registerRead(%p,%p,%lx,%lx,%lx)", i_chip.getChip(),
     //             o_buffer, io_bufSize, i_regType, i_address);
 
-    return rc;
+    return accessFailure;
 }
 
 //------------------------------------------------------------------------------
 
 #ifndef __HEI_READ_ONLY
 
-ReturnCode registerWrite(const Chip& i_chip, void* i_buffer, size_t& io_bufSize,
+bool registerWrite(const Chip& i_chip, void* i_buffer, size_t& io_bufSize,
                          uint64_t i_regType, uint64_t i_address)
 {
-    ReturnCode rc = RC_SUCCESS;
+    bool accessFailure = false;
 
     //    HEI_INF("registerWrite(%p,%p,%lx,%lx,%lx)", i_chip.getChip(),
     //             i_buffer, io_bufSize, i_regType, i_address);
 
-    return rc;
+    return accessFailure;
 }
 
 #endif
