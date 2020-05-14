c13481467cc1e992f26dde8054a0c22020346021
Ben Tyner <ben.tyner@ibm.com>
2020-05-14 17:56:54 -0500

    Hardware error analyzer for checkstop analyses
    When a checkstop occurs the attention handler will use the hardware
    error isolator to gather information pertaining to the error
    condition(s).
    
    Signed-off-by: Ben Tyner <ben.tyner@ibm.com>
    Change-Id: If933be6bddfc653edf0ed3db660c32d3aae04c5f

--- analyzer/analyzer_main.cpp
+++ analyzer/analyzer_main.cpp
@@ -1,42 +1,161 @@
+#include <libpdbg.h>
+
 #include <hei_main.hpp>
 
+#include <fstream>
+#include <iostream>
 #include <map>
 
 namespace analyzer
 {
 
-/** Analyze error condition using the hardware error isolator */
-bool analyzeHardware(std::map<std::string, std::string>& i_errors)
+// FIXME TEMP CODE - begin
+
+static constexpr uint32_t chipTypeP10      = 0;
+static constexpr uint32_t chipTypeExplorer = 1;
+
+// FIXME TEMP CODE - end
+
+/**
+ * @brief send chip data file to isolator
+ *
+ * Read a chip data file into memory and then send it to the isolator via
+ * the initialize interface.
+ *
+ * @param i_filePath The file path and name to read into memory
+ *
+ * @return Returns true if the isolator was successfully initialized with
+ *         a single chip data file. Returns false otherwise.
+ *
+ */
+bool initWithFile(const char* i_filePath)
 {
     using namespace libhei;
 
-    std::vector<Chip> chipList; // data to isolator
-    IsolationData isoData{};    // data from isolator
+    bool rc = true; // assume success
 
-    // FIXME TEMP CODE - start
+    // open the file and seek to the end to get length
+    std::ifstream fileStream(i_filePath, std::ios::binary | std::ios::ate);
 
-    initialize(nullptr, 0);
-
-    chipList.emplace_back(Chip{"proc", static_cast<ChipType_t>(0xdeadbeef)});
-
-    isolate(chipList, isoData); // isolate errors
-
-    if (!(isoData.getSignatureList().empty()))
+    if (!fileStream)
     {
-        // Signature signature = isoData.getSignatureList().back();
+        std::cout << "could not open file" << std::endl;
+        rc = false;
     }
     else
     {
-        std::string signature = "0xfeed";
-        std::string chip      = "0xbeef";
-        i_errors[signature]   = chip;
+        // get file size based on seek position
+        std::ifstream::pos_type fileSize = fileStream.tellg();
+
+        // create a buffer large enough to hold the entire file
+        std::vector<char> fileBuffer(fileSize);
+
+        // seek to the beginning of the file
+        fileStream.seekg(0, std::ios::beg);
+
+        // read the entire file into the buffer
+        fileStream.read(fileBuffer.data(), fileSize);
+
+        // done with the file
+        fileStream.close();
+
+        // intialize the isolator with the chip data
+        initialize(fileBuffer.data(), fileSize); // hei initialize
     }
 
-    uninitialize();
-
-    // FIXME TEMP CODE - end
-
-    return true; // FIXME - error/success from isolator or isolation data
+    return rc;
 }
+
+/**
+ * @brief Analyze using the hardware error isolator
+ *
+ * Query the hardware for each active chip that is a valid candidate for
+ * error analyses. Based on the list of active chips initialize the
+ * isolator with the associated chip data files. Finally request analyses
+ * from the hardware error isolator and log the results.
+ *
+ * @param o_errors A map for storing information about erros that were
+ *                 detected by the hardware error isolator.
+ *
+ * @return True if hardware error analyses was successful, false otherwise
+ */
+bool analyzeHardware(std::map<std::string, std::string>& o_errors)
+{
+    using namespace libhei;
+
+    bool rc = true;
+
+    std::vector<Chip> chipList; // chips that need to be analyzed
+
+    IsolationData isoData{}; // data from isolato
+
+    pdbg_target *targetProc, *targetOcmb; // P10 and explorer targets
+
+    pdbg_targets_init(NULL); // initialize targets
+
+    /** @brief gather list of chips to analyze */
+    pdbg_for_each_class_target("proc", targetProc)
+    {
+        // add each processor chip to the chip list
+        chipList.emplace_back(Chip(targetProc, chipTypeP10));
+
+        if (PDBG_TARGET_ENABLED == pdbg_target_probe(targetProc))
+        {
+            pdbg_for_each_compatible(targetProc, targetOcmb, "ibm,power10-ocmb")
+            {
+                if (PDBG_TARGET_ENABLED == pdbg_target_probe(targetOcmb))
+                {
+                    // add each explorer chip (ocmb) to the chip list
+                    chipList.emplace_back(Chip(targetOcmb, chipTypeExplorer));
+                }
+            }
+        }
+    }
+
+    // TODO select chip data files based on chip types detected
+    do
+    {
+        // TODO for now chip data files are local
+        // hei initialize
+        if (false == initWithFile("/usr/share/openpower-hw-diags/p10.cdb"))
+        {
+            rc = false;
+            break;
+        }
+
+        // TODO for now chip data files are local
+        // hei initialize
+        if (false == initWithFile("/usr/share/openpower-hw-diags/explorer.cdb"))
+        {
+            rc = false;
+            break;
+        }
+
+        // hei isolate
+        isolate(chipList, isoData);
+
+        if (!(isoData.getSignatureList().empty()))
+        {
+            // TODO parse signature list
+            int numErrors = isoData.getSignatureList().size();
+
+            std::cout << "isolated: " << numErrors << std::endl;
+        }
+        else
+        {
+            // FIXME TEMP CODE - begin
+
+            o_errors["0xfeed"] = "0xbeef"; // [signature] = chip
+
+            // FIXME TEMP CODE - end
+        }
+
+        // hei uninitialize
+        uninitialize();
+
+    } while (0);
+
+    return rc;
+} // namespace analyzer
 
 } // namespace analyzer
