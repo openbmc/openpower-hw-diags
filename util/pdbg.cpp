302f00ba546cf91126961fbf44f864f4f4d0fa00
Zane Shelley <zshelle@us.ibm.com>
2020-11-04 21:20:11 -0600

    pdbg utils for finding all active chips
    Change-Id: I444c1062d28c10ecd47426c60ffe0604cc5eaac5
    Signed-off-by: Zane Shelley <zshelle@us.ibm.com>

--- util/pdbg.cpp
+++ util/pdbg.cpp
@@ -2,6 +2,7 @@
 #include <libpdbg.h>
 
 #include <util/pdbg.hpp>
+#include <util/trace.hpp>
 
 namespace util
 {
@@ -11,9 +12,105 @@ namespace pdbg
 
 //------------------------------------------------------------------------------
 
+const char* __getPath(pdbg_target* i_trgt)
+{
+    return pdbg_target_path(i_trgt);
+}
+
 const char* getPath(const libhei::Chip& i_chip)
 {
-    return pdbg_target_path((pdbg_target*)i_chip.getChip());
+    return __getPath((pdbg_target*)i_chip.getChip());
+}
+
+//------------------------------------------------------------------------------
+
+uint32_t __getChipPos(pdbg_target* i_trgt)
+{
+    uint32_t attr = 0;
+    pdbg_target_get_attribute(i_trgt, "ATTR_FAPI_POS", 4, 1, &attr);
+    return attr;
+}
+
+uint32_t getChipPos(const libhei::Chip& i_chip)
+{
+    return __getChipPos((pdbg_target*)i_chip.getChip());
+}
+
+//------------------------------------------------------------------------------
+
+uint32_t __getChipId(pdbg_target* i_trgt)
+{
+    uint32_t attr = 0;
+    pdbg_target_get_attribute(i_trgt, "ATTR_CHIP_ID", 4, 1, &attr);
+    return attr;
+}
+
+uint8_t __getChipEc(pdbg_target* i_trgt)
+{
+    uint8_t attr = 0;
+    pdbg_target_get_attribute(i_trgt, "ATTR_EC", 1, 1, &attr);
+    return attr;
+}
+
+uint32_t __getChipIdEc(pdbg_target* i_trgt)
+{
+    return ((__getChipId(i_trgt) & 0xffff) << 16) | __getChipEc(i_trgt);
+}
+
+//------------------------------------------------------------------------------
+
+void __addChip(std::vector<libhei::Chip>& o_chips, pdbg_target* i_trgt,
+               libhei::ChipType_t i_type)
+{
+    // Trace each chip for debug. It is important to show the type just in case
+    // the model/EC does not exist. See note below.
+    trace::inf("Chip found: type=0x%08" PRIx32 " chip=%s", i_type,
+               __getPath(i_trgt));
+
+    if (0 == i_type)
+    {
+        // There are two reasons for this:
+        //  - Due to some design limitation in libpdbg, all processors are
+        //    considered active, even if they do not exist.
+        //  - There is a special case where the model/level attributes have not
+        //    been initialized in the devtree. This is possible on the epoch IPL
+        //    where an attention occurs before Hostboot is able to update the
+        //    devtree information on the BMC.
+        // In both cases, just ignore the chip.
+    }
+    else
+    {
+        o_chips.emplace_back(i_trgt, i_type);
+    }
+}
+
+void getActiveChips(std::vector<libhei::Chip>& o_chips)
+{
+    o_chips.clear();
+
+    // Iterate each processor.
+    pdbg_target* procTrgt;
+    pdbg_for_each_class_target("proc", procTrgt)
+    {
+        // Active processors only.
+        if (PDBG_TARGET_ENABLED != pdbg_target_probe(procTrgt))
+            continue;
+
+        // Add the processor to the list.
+        __addChip(o_chips, procTrgt, __getChipIdEc(procTrgt));
+
+        // Iterate the connected OCMBs, if they exist.
+        pdbg_target* ocmbTrgt;
+        pdbg_for_each_target("ocmb", procTrgt, ocmbTrgt)
+        {
+            // Active OCMBs only.
+            if (PDBG_TARGET_ENABLED != pdbg_target_probe(ocmbTrgt))
+                continue;
+
+            // Add the OCMB to the list.
+            __addChip(o_chips, ocmbTrgt, __getChipIdEc(ocmbTrgt));
+        }
+    }
 }
 
 //------------------------------------------------------------------------------
--- util/pdbg.hpp
+++ util/pdbg.hpp
@@ -14,6 +14,18 @@ namespace pdbg
  */
 const char* getPath(const libhei::Chip& i_chip);
 
+/**
+ * @param  A chip.
+ * @return The absolute position of the given chip.
+ */
+uint32_t getChipPos(const libhei::Chip& i_chip);
+
+/**
+ * @brief Returns the list of all active chips in the system.
+ * @param o_chips The returned list of chips.
+ */
+void getActiveChips(std::vector<libhei::Chip>& o_chips);
+
 } // namespace pdbg
 
 } // namespace util
