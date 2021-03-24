e6b47cf61d328463bdb011d075b95cfdd915d6e2
Zane Shelley <zshelle@us.ibm.com>
2021-03-24 17:07:46 -0500

    Implemented HardwareCalloutResolution::resolve()
    Change-Id: I19a0d1da6b172fd74fab8680c515391b883dac95

--- analyzer/resolution.cpp
+++ analyzer/resolution.cpp
@@ -1,11 +1,28 @@
 #include <analyzer/resolution.hpp>
+#include <util/pdbg.hpp>
 
 namespace analyzer
 {
 
-void HardwareCalloutResolution::resolve(ServiceData&) const
+void HardwareCalloutResolution::resolve(ServiceData& io_sd) const
 {
-    // TODO: Add location code to callout list and entity path to gard list.
+    // Get the chip target from the root cause signature.
+    auto trgt = util::pdbg::getTrgt(io_sd.getRootCause().getChip());
+
+    // Get the unit target, if needed.
+    if (!iv_path.empty())
+    {
+        std::string path{util::pdbg::getPath(trgt)};
+        trgt = util::pdbg::getTrgt(path + "/" + iv_path);
+    }
+
+    // Add location code to callout list.
+    auto locCode = util::pdbg::getLocationCode(trgt);
+    io_sd.addCallout(std::make_shared<HardwareCallout>(locCode, iv_priority));
+
+    // Add entity path to gard list.
+    auto entityPath = util::pdbg::getPhysDevPath(trgt);
+    io_sd.addGuard(std::make_shared<Guard>(entityPath, iv_guard));
 }
 
 } // namespace analyzer
