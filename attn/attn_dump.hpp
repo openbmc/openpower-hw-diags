88803573fe570a7186c3d39847629342e4d4ca62
Ben Tyner <ben.tyner@ibm.com>
2021-08-09 19:18:24 -0500

    On checkstop request dump and transition host
    After checkstop analyses completes attention handler will request a dump
    and the transition the host. The type of dump requested is dependent on
    the results of the isolator.
    
    Signed-off-by: Ben Tyner <ben.tyner@ibm.com>
    Change-Id: I1116be8f8d045a4916d3321f461c22345fecf274

---
+++ attn/attn_dump.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+namespace attn
+{
+
+/** @brief Dump types supported by dump request */
+enum class DumpType
+{
+    Hostboot,
+    Hardware
+};
+
+/** @brief Structure for dump request parameters */
+class DumpParameters
+{
+  public:
+    uint32_t logId;
+    uint32_t unitId;
+    DumpType dumpType;
+};
+
+/**
+ * Request a dump from the dump manager
+ *
+ * Request a dump from the dump manager and register a monitor for observing
+ * the dump progress.
+ *
+ * @param dumpParameters Parameters for the dump request
+ */
+void requestDump(const DumpParameters dumpParameters);
+
+} // namespace attn
