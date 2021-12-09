53c97c833669f88614f87e5c52ba129017674aa5
austinfcui <austinfcui@gmail.com>
2021-12-09 17:56:00 -0600

    Update test/meson.build for pdbg lib dependency
    3. Updated meson_options.txt file to enable phal.
    2. Add /test/pdbg_dts_test.dts as source of input file to UT case.
    1. Update test/meson.build to include library dependency in test cases
    
    Signed-off-by: austinfcui <austinfcui@gmail.com>
    Change-Id: I9a82bb954e0a329691579a10fdf7e71c229d1e73

---
+++ test/pdbg_dts_test.cpp
@@ -0,0 +1,53 @@
+#include <fcntl.h>
+
+#include <util/pdbg.hpp>
+#include <util/trace.hpp>
+
+#include <limits>
+
+#include "gtest/gtest.h"
+
+TEST(PDBG, PdbgDtsTest1)
+{
+    const uint32_t chipId  = 0; // ID for proc0.
+    const uint32_t fapiPos = 0; // FAPI Position proc0.
+
+    pdbg_targets_init(nullptr);
+
+    // Iterate each processor.
+    pdbg_target* procTrgt;
+    pdbg_for_each_class_target("proc", procTrgt)
+    {
+        // Active processors only.
+        if (PDBG_TARGET_ENABLED !=
+            pdbg_target_probe(util::pdbg::getPibTrgt(procTrgt)))
+            continue;
+
+        // Process the PROC target.
+        // Display Proccessor info in the target
+        uint32_t attr = std::numeric_limits<uint32_t>::max();
+        pdbg_target_get_attribute(procTrgt, "ATTR_CHIP_ID", 4, 1, &attr);
+        trace::inf("Chip ID: %u", attr);
+        ASSERT_EQ(attr, chipId);
+
+        // Display Proccessor info in the target
+        attr = std::numeric_limits<uint32_t>::max();
+        pdbg_target_get_attribute(procTrgt, "ATTR_FAPI_POS", 4, 1, &attr);
+        trace::inf("ATTR_FAPI_POS: %u", attr);
+        EXPECT_EQ(attr, fapiPos);
+
+        // Iterate the PIB, if they exist.
+        pdbg_target* pibTrgt;
+        pdbg_for_each_target("pib", procTrgt, pibTrgt)
+        {
+            // Active PIB only.
+            if (PDBG_TARGET_ENABLED != pdbg_target_probe(pibTrgt))
+                continue;
+
+            // Process the PIB.
+            attr = std::numeric_limits<uint32_t>::max();
+            pdbg_target_get_attribute(procTrgt, "ATTR_FAPI_POS", 4, 1, &attr);
+            trace::inf("ATTR_FAPI_POS: %u", attr);
+        }
+    }
+}
