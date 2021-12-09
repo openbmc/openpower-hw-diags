a6fd9c995048946054d649a579dc611fc9512c23
austinfcui <austinfcui@gmail.com>
2021-12-09 17:56:00 -0600

    Update test/meson.build for pdbg lib dependency
    Update test/meson.build to include pdbg library dependency in test cases
    
    Signed-off-by: austinfcui <austinfcui@gmail.com>
    Change-Id: I9a82bb954e0a329691579a10fdf7e71c229d1e73

---
+++ test/pdbg_dts_test.cpp
@@ -0,0 +1,8 @@
+// TODO: add other header files later.
+#include "gtest/gtest.h"
+
+TEST(PDBG, PdbgDtsTest1)
+{
+    // TODO: need to add test statements later.
+    // after pdbg functions are implemented.
+}
