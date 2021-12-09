a0ca21108a7c85f34eb995610e44c18bbe9fb6ce
austinfcui <austinfcui@gmail.com>
2021-12-09 17:56:00 -0600

    Update test/meson.build for pdbg lib dependency
    2. Add /test/pdbg_dts_test.dts as input file to UT case.
    1. Update test/meson.build to include pdbg library dependency in test cases
    
    Signed-off-by: austinfcui <austinfcui@gmail.com>
    Change-Id: I9a82bb954e0a329691579a10fdf7e71c229d1e73

---
+++ test/pdbg_dts_test.cpp
@@ -0,0 +1,10 @@
+// TODO: add other header files later.
+#include "gtest/gtest.h"
+
+TEST(PDBG, PdbgDtsTest1)
+{
+    // TODO: need to add test statements later.
+    // after pdbg functions are implemented.
+
+    pdbg_targets_init(nullptr); // nullptr == use default fdt
+}
