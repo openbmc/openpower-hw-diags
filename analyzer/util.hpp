63c4debf7240ed4bd4d6d672cc2b81cb37301176
Zane Shelley <zshelle@us.ibm.com>
2021-12-16 22:32:54 -0600

    Move register/isolation node hash to util header
    Signed-off-by: Zane Shelley <zshelle@us.ibm.com>
    Change-Id: Ie36fb1d6333fd8834bf0dfc3194441bd8b100746

--- analyzer/util.hpp
+++ analyzer/util.hpp
@@ -34,4 +34,60 @@ inline BinFileWriter& BinFileWriter::operator<<(libhei::RegisterId_t r)
     return *this;
 }
 
+//------------------------------------------------------------------------------
+
+/**
+ * @brief The string hashing algorithm used for register and isolation node
+ *        names.
+ */
+inline uint64_t hash(unsigned int i_bytes, const std::string& i_str)
+{
+    // This hash is a simple "n*s[0] + (n-1)*s[1] + ... + s[n-1]" algorithm,
+    // where s[i] is a chunk from the input string the length of i_bytes.
+
+    // Currently only supporting 1-8 byte hashes.
+    assert(1 <= i_bytes && i_bytes <= sizeof(uint64_t));
+
+    // Start hashing each chunk.
+    uint64_t sumA = 0;
+    uint64_t sumB = 0;
+
+    // Iterate one chunk at a time.
+    for (unsigned int i = 0; i < i_str.size(); i += i_bytes)
+    {
+        // Combine each chunk into a single integer value. If we reach the end
+        // of the string, pad with null characters.
+        uint64_t chunk = 0;
+        for (unsigned int j = 0; j < i_bytes; j++)
+        {
+            chunk <<= 8;
+            chunk |= (i + j < i_str.size()) ? i_str[i + j] : '\0';
+        }
+
+        // Apply the simple hash.
+        sumA += chunk;
+        sumB += sumA;
+    }
+
+    // Mask off everything except the target number of bytes.
+    auto mask = std::numeric_limits<uint64_t>::max();
+    sumB &= mask >> ((sizeof(uint64_t) - i_bytes) * 8);
+
+    return sumB;
+}
+
+template <class T>
+inline uint64_t hash(const std::string& i_str)
+{
+    return hash(sizeof(T), i_str);
+}
+
+template <>
+inline uint64_t hash<libhei::RegisterId_t>(const std::string& i_str)
+{
+    return hash(3, i_str);
+}
+
+//------------------------------------------------------------------------------
+
 } // namespace util
