727368a6674f2f0ee8a52330b7ac4ff284184a91
Pavithra Barithaya <pavithrabarithaya07@gmail.com>
2024-10-14 15:37:53 +0530

    Move to libpldm instance id APIs
    libpldm provides APIs for allocating instance IDs directly, which
    eliminates the need for remote dbus calls to the pldm daemon.  Refactor
    the code to use these APIs and eliminate all the dbus operations.
    
    Change-Id: Ie73a1abb30e12a40014b2d35209e6e90c38a148b
    Signed-off-by: Pavithra Barithaya <pavithrabarithaya07@gmail.com>

--- util/pldm.hpp
+++ util/pldm.hpp
@@ -1,9 +1,54 @@
 #pragma once
 
+#include <libpldm/instance-id.h>
+
 namespace util
 {
 namespace pldm
 {
+class PLDMInstanceManager
+{
+  public:
+    PLDMInstanceManager(const PLDMInstanceManager&) = delete;
+    PLDMInstanceManager& operator=(const PLDMInstanceManager&) = delete;
+    PLDMInstanceManager();
+    ~PLDMInstanceManager();
+
+  private:
+    /**
+     * @brief Instantiates an instance ID database object
+     *
+     * @return void
+     **/
+    void initPLDMInstanceIdDb();
+    /**
+     * @brief Destroys an instance ID database object
+     *
+     * @return void
+     **/
+    void destroyPLDMInstanceIdDb();
+};
+
+/**
+ * @brief Get PLDM  instance ID associated with endpoint
+ *
+ * @param[out] pldmInstance - PLDM instance id
+ * @param[in] tid - the terminus ID the instance ID is associated with
+ *
+ * @return True on success otherwise False
+ */
+bool getPldmInstanceID(uint8_t& pldmInstance, uint8_t tid);
+
+/**
+ * @brief Free the PLDM instance ID
+ *
+ * @param[in] tid - the terminus ID the instance ID is associated with
+ * @param[in] instanceID - The PLDM instance ID
+ *
+ * @return void
+ **/
+void freePLDMInstanceID(pldm_instance_id_t instanceID, uint8_t tid);
+
 /*
  *  @brief HRESET the SBE
  *
@@ -15,6 +60,10 @@ namespace pldm
  *
  */
 bool hresetSbe(unsigned int sbeInstance);
+
+/** @brief PLDM instance number used in PLDM requests
+ */
+uint8_t pldmInstanceID;
 
 } // namespace pldm
 } // namespace util
